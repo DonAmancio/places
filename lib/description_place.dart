d8ebeb116b2d76c0d4c44698e72c4ce00bc78424
DonAmancio <70178600+DonAmancio@users.noreply.github.com>
2021-07-13 20:51:10 -0400

    quinta claswe

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,85 +1,166 @@
-import 'dart:html';
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget{
+
+
+  //variables
+  String textoTitulo;
+  int cantidadEstrellas;
+  String TextoDescription;
+
+
+
+
+  DescriptionPlace(this.textoTitulo, this.cantidadEstrellas,this.TextoDescription);
+
+
+
   @override
   Widget build(BuildContext context) {
-    final titulo = Container(
+
+    final titulo = Container (
+
       margin: EdgeInsets.only(
-        right: 20,
+
+          right: 20
+
+
       ),
+
       child: Text(
-        "Duwili Ella",
+
+        textoTitulo,
+
         style: TextStyle(
-            fontSize: 40,
+
+            fontFamily: "Lato",
+
+            fontSize: 27,
             fontWeight: FontWeight.bold
+
+
         ),
+
       ),
+
     );
 
     final estrella = Container(
+
       margin: EdgeInsets.only(
-        right: 5,
+
+          right: 5
 
       ),
+
       child: Icon(
+
         Icons.star,
         color: Colors.amber,
       ),
+
     );
 
-    final estrellaBorde= Container(
-      child: Icon(
-        Icons.star_border,
-        color: Colors.black54,
+    final estrellaBorde = Container(
+
+      margin: EdgeInsets.only(
+
+          right: 5
+
       ),
 
+      child: Icon(
+
+        Icons.star_border,
+        color:  Colors.black54,
+
+      ),
 
     );
 
-    final filaEstrellas = Row(
-      children: <Widget>[
-        estrella,
-        estrella,
-        estrella,
-        estrella,
-        estrella,
-      ],
 
+    //fila de estrellas
+
+
+    List<Container> estrellas = new List();
+
+    for(int i = 0; i < 5; i++){
+
+      if(i < cantidadEstrellas){
+
+        estrellas.add(estrella);
+
+
+      }
+      else{
+
+        estrellas.add(estrellaBorde);
+
+      }
+
+    }
+
+    final filaEstrellas = Row(
+
+      children: estrellas,
 
     );
 
     final filaTitulo = Row(
-      children: <Widget>[
+
+      children: <Widget> [
+
         titulo,
-        filaEstrellas,
+
+        filaEstrellas
+
 
       ],
 
     );
+
+
     final descripcion = Container(
-      margin:  EdgeInsets.only(
-          top: 10
+
+      margin: EdgeInsets.only(
+
+          top: 15
+
       ),
+
       child: Text(
-        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+
+        TextoDescription,
+
         style: TextStyle(
-          color: Colors.black54,
+
+            fontFamily: "Lato",
+
+            color: Colors.black54
+
         ),
+
       ),
+
     );
 
     final descriptionPlace = Column(
-      children: <Widget>[
+
+      children: <Widget> [
+
         filaTitulo,
-        descripcion,
+        descripcion
+
       ],
+
+
     );
-    return null;
+
+    return descriptionPlace;
+
   }
+
+
 
 
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,35 +1,77 @@
-import 'dart:html';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:placesproyect/review.dart';
 
-import 'description_place.dart';
+import 'package:placesproyect/description_place.dart';
 
-class MyHome extends StatelessWidget{
+class MyHome extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context){
-    final descriptionPlace = Container(
+
+  Widget build(BuildContext context) {
+
+    final Container descriptionPlace = Container(
+
       margin: EdgeInsets.only(
-        top: 250,
-        left: 30,
-        right: 30,
+
+
+          top: 350,
+          left: 20,
+          right: 16
+
       ),
 
 
+      child: DescriptionPlace("salar de Uyuni", 5, "El Salar de Uyuni, en medio de los Andes en el sur de Bolivia, es la salina más grande del mundo. Es el legado de un lago prehistórico que se secó y dejó un paisaje desértico de casi 11,000 km cuadrados de sal blanca brillante, formaciones rocosas e islas con cactus. Su mística extensión se puede observar desde la isla central Incahuasi. Aunque la fauna es escasa en este ecosistema único, alberga a varios flamencos rosa "),
+
 
     );
+
+
+    final review = Container(
+
+        margin: EdgeInsets.only(
+
+
+            top: 350,
+            left: 20,
+            right: 20
+
+        ),
+
+
+        height: 80,
+
+        child: Review("assets/images/perfil.jpg","KUNIO", "1 reviews - 4 photos" , 2 , "Exelente lugar para los pibes.")
+
+    );
+
 
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My Places"),
+
+      appBar: AppBar(
+
+        title: Text(
+
+
+            "My places"
+
         ),
-        body: Stack(
-          children: <Widget>[
 
-          ],
-        )
+
+
+      ),
+
+      body: Stack(
+
+        children: <Widget> [
+
+          review
+
+        ],
+
+      ),
+
     );
-  }
 
-}
+  }
